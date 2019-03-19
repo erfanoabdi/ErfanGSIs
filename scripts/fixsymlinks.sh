c91fe5f4bc3f289a4fed15649b4be7d11e900864
Erfan Abdi <erfangplus@gmail.com>
2019-03-19 02:26:56 +0200

    use ls to fix symlinks
    Signed-off-by: Erfan Abdi <erfangplus@gmail.com>

--- scripts/fixsymlinks.sh
+++ scripts/fixsymlinks.sh
@@ -6,11 +6,11 @@ boot-framework-oahl-backward-compatibility.vdex
 
 for partition in "${partitions[@]}"
 do
-    if [[ -e "$1/framework/arm64/$partition" ]]; then
+    if [[ $(ls "$1/framework/arm64/" | grep "$partition") ]]; then
         rm -rf "$1/framework/arm64/$partition"
         ln -s "/system/framework/$partition" "$1/framework/arm64/$partition"
     fi
-    if [[ -e "$1/framework/arm/$partition" ]]; then
+    if [[ $(ls "$1/framework/arm/" | grep "$partition") ]]; then
         rm -rf "$1/framework/arm/$partition"
         ln -s "/system/framework/$partition" "$1/framework/arm/$partition"
     fi
