89e7bc912cab1626d9003d1b12058b7aabc46f48
Erfan Abdi <erfangplus@gmail.com>
2019-03-19 05:30:53 +0200

    add ZUI support
    Signed-off-by: Erfan Abdi <erfangplus@gmail.com>

---
+++ roms/9/ZUI/make.sh
@@ -0,0 +1,11 @@
+#/bin/bash
+
+systempath=$1
+thispath=`cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd`
+
+# AOSP libs
+cp -fpr $thispath/lib64/* $1/lib64/
+
+# remove phh qtiaudio
+rm -rf $1/priv-app/QtiAudio
+rm -rf $1/app/NQNfcNci
