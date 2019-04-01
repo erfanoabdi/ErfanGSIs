8a82903afdaf24a1a93b96049cd28b36fdf352b8
Erfan Abdi <erfangplus@gmail.com>
2019-04-01 20:58:01 +0300

    drop qcom stuffs for non qcom devices
    Signed-off-by: Erfan Abdi <erfangplus@gmail.com>

--- prebuilt/google2phh/rw-system.add.sh
+++ prebuilt/google2phh/rw-system.add.sh
@@ -6,3 +6,19 @@ mount -o bind /system/lib64/vndk-"$vndk"/libgui.so /vendor/lib64/libgui_vendor.s
 
 mount -o bind system/phh/empty /vendor/lib/libbinder.so || true
 mount -o bind system/phh/empty /vendor/lib64/libbinder.so || true
+
+# drop qcom location for mi mix 3
+if getprop ro.vendor.build.fingerprint | grep -iq \
+    -e iaomi/perseus/perseus;then
+    mount -o bind /mnt/phh/empty_dir /system/priv-app/com.qualcomm.location || true
+fi
+
+# drop qcom stuffs for non qcom devices
+if ! getprop ro.hardware | grep -qiE -e qcom -e mata;then
+    mount -o bind /mnt/phh/empty_dir /system/priv-app/com.qualcomm.location || true
+    mount -o bind /mnt/phh/empty_dir /system/app/imssettings || true
+    mount -o bind /mnt/phh/empty_dir /system/priv-app/ims || true
+    mount -o bind /mnt/phh/empty_dir /system/app/ims || true
+    mount -o bind /mnt/phh/empty_dir /system/app/QtiTelephonyService || true
+    mount -o bind /mnt/phh/empty_dir /system/app/datastatusnotification || true
+fi
