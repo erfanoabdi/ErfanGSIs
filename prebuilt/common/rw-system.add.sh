552ae9c83a38d70c49afa76bb2bc73fc0a5896b8
Erfan Abdi <erfangplus@gmail.com>
2019-04-04 20:05:29 +0300

    fix broken in vendor libbinder.so
    Signed-off-by: Erfan Abdi <erfangplus@gmail.com>

--- prebuilt/common/rw-system.add.sh
+++ prebuilt/common/rw-system.add.sh
@@ -4,8 +4,8 @@
 mount -o bind /system/lib/vndk-"$vndk"/libgui.so /vendor/lib/libgui_vendor.so || true
 mount -o bind /system/lib64/vndk-"$vndk"/libgui.so /vendor/lib64/libgui_vendor.so || true
 
-mount -o bind system/phh/empty /vendor/lib/libbinder.so || true
-mount -o bind system/phh/empty /vendor/lib64/libbinder.so || true
+mount -o bind /system/lib/vndk-"$vndk"/libbinder.so /vendor/lib/libbinder.so || true
+mount -o bind /system/lib64/vndk-"$vndk"/libbinder.so /vendor/lib64/libbinder.so || true
 
 # drop qcom location for mi mix 3
 if getprop ro.vendor.build.fingerprint | grep -iq \
