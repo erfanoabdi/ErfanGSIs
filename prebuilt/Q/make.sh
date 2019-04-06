436cac838e60f00d84eca43a0ef5fa2356ce7c1b
Erfan Abdi <erfangplus@gmail.com>
2019-04-06 16:09:07 +0300

    add some phh overlays to Q
    Signed-off-by: Erfan Abdi <erfangplus@gmail.com>

--- prebuilt/Q/make.sh
+++ prebuilt/Q/make.sh
@@ -14,6 +14,7 @@ cp -fpr $thispath/ld/* $1/etc/
 cp -fpr $thispath/init/* $1/etc/init/
 cp -fpr $thispath/permissions/* $1/etc/permissions/
 #cp -fpr $thispath/app/* $1/app/
+cp -fpr $thispath/overlay $1/product/
 
 cat $thispath/rw-system.add.sh >> $1/bin/rw-system.sh
 echo "persist.bluetooth.bluetooth_audio_hal.disabled=true" >> $1/build.prop
