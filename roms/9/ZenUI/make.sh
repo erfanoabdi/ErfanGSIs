962d6d4f207d6552640b6005b167e9672522ccc1
Erfan Abdi <erfangplus@gmail.com>
2019-05-26 15:05:10 +0300

    cleanup ZenUI
    Signed-off-by: Erfan Abdi <erfangplus@gmail.com>

--- roms/9/ZenUI/make.sh
+++ roms/9/ZenUI/make.sh
@@ -4,17 +4,4 @@ systempath=$1
 thispath=`cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd`
 
 # build.prop
-#cp -fpr $thispath/build.prop $1/
 echo "ro.bluetooth.library_name=libbluetooth_qti.so" >> $1/build.prop
-
-# drop caf permissions
-rm -rf $1/etc/permissions/qti_permissions.xml
-sed -i "s/persist.sys.usb.config=none/persist.sys.usb.config=adb/g" $1/etc/prop.default
-echo "ro.setupwizard.mode=DISABLED" >> $1/etc/prop.default
-# remove phh qtiaudio
-rm -rf $1/priv-app/QtiAudio
-rm -rf $1/app/NQNfcNci
-#rm -rf $1/app/Bluetooth
-
-# remove notch
-cp -fpr $thispath/framework-res.apk $1/framework/
