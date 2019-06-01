3fee06b98ada675c872ee39384dc23047b0aa7d0
Erfan Abdi <erfangplus@gmail.com>
2019-06-01 19:34:23 +0300

    fix OOS ril
    Signed-off-by: Erfan Abdi <erfangplus@gmail.com>

--- roms/9/OxygenOS/make.sh
+++ roms/9/OxygenOS/make.sh
@@ -12,6 +12,7 @@ cp -fpr $thispath/init/* $1/etc/init/
 cp -fpr $thispath/priv-app/* $1/priv-app/
 #cp -fpr $thispath/manifest.xml $1/etc/vintf/
 cp -fpr $thispath/overlay/* $1/product/overlay/
+cp -fpr $thispath/framework/* $1/framework/
 
 python $thispath/../../../scripts/custom_manifest.py $thispath/../../../tmp/manifest.xml $thispath/manifest.xml $1/etc/vintf/manifest.xml
 cp -fpr $thispath/../../../tmp/manifest.xml $1/etc/vintf/manifest.xml
