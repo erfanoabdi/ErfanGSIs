e6f4330bbc5c00180479dc0cafbce8f65459a787
amackpro <48667677+amackpro@users.noreply.github.com>
2019-06-04 19:47:38 +0530

    Update debloat.sh
    Signed-off-by: Erfan Abdi <erfangplus@gmail.com>

--- roms/9/ColorOS/debloat.sh
+++ roms/9/ColorOS/debloat.sh
@@ -3,13 +3,41 @@
 systempath=$1
 thispath=`cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd`
 
-# Bloatware clean-up - Fit to 3GB
 rm -rf $1/app/BackupAndRestore
 rm -rf $1/app/BTtestmode
+rm -rf $1/app/Calculator2
+rm -rf $1/app/ChildrenSpace
+rm -rf $1/app/EmailPartnerProvider
 rm -rf $1/app/FindMyPhoneClient
 rm -rf $1/app/GameSpace
+rm -rf $1/app/Ocrscanner
+rm -rf $1/app/Oshare
 rm -rf $1/app/ModemTestMode
 rm -rf $1/app/NewSoundRecorder
 rm -rf $1/app/talkback
-rm -rf $1/priv-app/GmsCore
+rm -rf $1/app/FM2
+rm -rf $1/app/Maps
+rm -rf $1/app/Keep
+rm -rf $1/app/Gmail2
+rm -rf $1/app/GoogleTTS
+rm -rf $1/app/NewSoundRecorder
+rm -rf $1/app/OppoCamera
+rm -rf $1/app/OppoCompass2
+rm -rf $1/app/OppoDCS
+rm -rf $1/app/Youtube
+rm -rf $1/app/SecurePay
+rm -rf $1/app/SecureElement
+rm -rf $1/app/SafeCenter
+rm -rf $1/priv-app/Blacklist
+rm -rf $1/priv-app/BlacklistApp
 rm -rf $1/priv-app/TestApp5G
+rm -rf $1/priv-app/com.facebook.system
+rm -rf $1/priv-app/SmartDrive
+rm -rf $1/priv-app/OppoMusic
+rm -rf $1/priv-app/OppoBootReg
+rm -rf $1/priv-app/OppoGallery2
+rm -rf $1/priv-app/VideoGallery
+rm -rf $1/priv-app/Velvet
+rm -rf $1/priv-app/VideoGallery
+rm -rf $1/reserve
+
