33896e670db409bf73fd9ad227d7f3251222bcc6
Erfan Abdi <erfangplus@gmail.com>
2019-06-04 20:55:18 +0300

    ColorOS: add more fixes
    Signed-off-by: Erfan Abdi <erfangplus@gmail.com>

--- roms/9/ColorOS/bin/color.sh
+++ roms/9/ColorOS/bin/color.sh
@@ -1,6 +1,13 @@
+#!/system/bin/sh
+
+# Copyright (C) 2019 Erfan Abdi (erfangplus@gmail.com)
+
 mkdir -p /data/erf
 chmod 0775 /data/erf
 chown system:system /data/erf
 echo "SET_BOOTSTAGE@NATIVE_INIT_TRIGGER_POST_FS_DATA" > /data/erf/phx
 chmod 0640 /data/erf/phx
 chown system:system /data/erf/phx
+touch /data/erf/botfrm
+chmod 0640 /data/erf/botfrm
+chown system:system /data/erf/botfrm
--- roms/9/ColorOS/make.sh
+++ roms/9/ColorOS/make.sh
@@ -9,10 +9,24 @@ thispath=`cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd`
 rm -rf $1/priv-app/OppoBootReg
 cp -fpr $thispath/SetupWizard $1/priv-app/
 
+# Custom files
+cp -fpr $thispath/init/* $1/etc/init/
+cp -fpr $thispath/bin/* $1/bin/
+
+# drop QVRservice
+rm -rf $1/bin/qvrservice
+
+# Append file_context
+cat $thispath/file_contexts >> $1/etc/selinux/plat_file_contexts
+
+# Fix packageinstaller
+echo "ro.build.version.sdk=28" >> $1/build.prop
+
+# oppo hacks
 sed -i "s/vendor.oppo.hardware.cryptoeng/vendor.fuck.hardware.cryptoeng/g" $1/etc/vintf/manifest.xml
 
-# hack phoenix
 sed -i "s|/proc/phoenix|/data/erf/phx|g" $1/framework/boot-framework.vdex
 sed -i "s|/proc/phoenix|/data/erf/phx|g" $1/lib/libphoenix_native.so
 sed -i "s|/proc/phoenix|/data/erf/phx|g" $1/lib64/libphoenix_native.so
-cat $thispath/rw-system.add.sh >> $1/bin/rw-system.sh
+
+sed -i "s|/proc/opbootfrom|/data/erf/botfrm|g" $1/framework/oat/arm64/services.vdex
