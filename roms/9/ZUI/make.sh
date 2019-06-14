964fa546a8f476ff01d7f6860990a99ac532e26b
MacTavishAO <ataberkozen123@gmail.com>
2019-06-14 00:01:29 +0000

    roms: changes for Z6 Pro's ZUI 11

--- roms/9/ZUI/make.sh
+++ roms/9/ZUI/make.sh
@@ -4,7 +4,7 @@ systempath=$1
 thispath=`cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd`
 
 # AOSP libs
-cp -fpr $thispath/lib64/* $1/lib64/
+#cp -fpr $thispath/lib64/* $1/lib64/
 
 # remove phh qtiaudio
 rm -rf $1/priv-app/QtiAudio
