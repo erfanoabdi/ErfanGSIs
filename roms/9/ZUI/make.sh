4b501de305695f29a89b570224cffbb718a8430d
MacTavishAO <ataberkozen123@gmail.com>
2019-06-14 12:30:36 +0000

    ZUI: Workaround for SetupWizard While completing setup with 16:9 device, on last step "finish" button out of screen these devices gets stuck on last step and unable to complete Setup Wizard.

--- roms/9/ZUI/make.sh
+++ roms/9/ZUI/make.sh
@@ -6,6 +6,14 @@ thispath=`cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd`
 # AOSP libs
 #cp -fpr $thispath/lib64/* $1/lib64/
 
+# Workaround for SetupWizard Problem
+# Remove ZUI's SetupWizard and push GoogleSetupWizard
+# Then disable SetupWizard with prop.
+# ZUI's SetupWizard out of screen on 16:9 devices.
+# These devices cannot finish setupwizard and gets stuck.
+rm -rf $1/priv-app/ZuiSetupWizard
+cp -fpr $thispath/SetupWizard $1/priv-app/
+
 # remove phh qtiaudio
 rm -rf $1/priv-app/QtiAudio
 rm -rf $1/app/NQNfcNci
