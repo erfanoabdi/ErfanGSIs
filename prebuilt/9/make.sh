d3d569297b8721e3ab4b4605f8c9c5e36b5cb753
Erfan Abdi <erfangplus@gmail.com>
2019-06-29 13:53:58 +0300

    OxygenOS: Don’t copy device overlays for oos
    Signed-off-by: Erfan Abdi <erfangplus@gmail.com>

--- prebuilt/9/make.sh
+++ prebuilt/9/make.sh
@@ -5,12 +5,12 @@ romdir=$2
 thispath=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
 # Apply overlay patches
-$thispath/overlays/make.sh "$systempath"
+$thispath/overlays/make.sh "$systempath" "$romdir"
 
 # Brightness fix for Android P
 # But some systems are using custom light services, don't apply this patch on those roms
 if [ -f $romdir/DONTPATCHLIGHT ]; then
-    echo "Patching lights on $romtype isn't working. Skipping..."
+    echo "Patching lights for brightness fix is not supported in this rom. Skipping..."
 else
     $thispath/brightnessfix/make.sh "$systempath"
 fi
