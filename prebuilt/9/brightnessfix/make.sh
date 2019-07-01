dba5d4ab868c71d46db59b0b79d7b0b0df1ee20c
Yuhan Zhang <yuhan@rsyhan.me>
2019-07-01 18:52:54 +0800

    9: BrightnessFix: Get max brightness from roms automaticly
    Signed-off-by: Yuhan Zhang <yuhan@rsyhan.me>

--- prebuilt/9/brightnessfix/make.sh
+++ prebuilt/9/brightnessfix/make.sh
@@ -18,4 +18,8 @@ java -jar "$BAKSMALIJAR" disassemble "$TMPDIR/original_dex/classes.dex" -o "$TMP
 cp -fpr "$LOCALDIR"/com_android_server_lights/* "$TMPDIR"/dexout/com/android/server/lights/
 java -jar "$SMALIJAR" assemble "$TMPDIR/dexout" -o "$TMPDIR/classes.dex"
 zip -gjq "$systempath/framework/services.jar" "$TMPDIR/classes.dex"
+java -jar $toolsdir/apktool/apktool.jar d $systempath/framework/framework-res.apk -o $TMPDIR/framework-res
+cd $TMPDIR/framework-res/res/values
+ROMMAXBRIGHTNESS=$(grep '<integer name="config_screenBrightnessSettingMaximum">' integers.xml | sed 's/<integer name="config_screenBrightnessSettingMaximum">//g' | sed 's/<\/integer>//' | sed 's/^[[:space:]]*//g')
+echo "persist.display.rom_max_brightness=$ROMMAXBRIGHTNESS" >> $systempath/build.prop
 rm -rf "$TMPDIR"
