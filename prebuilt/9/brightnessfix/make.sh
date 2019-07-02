e55cc251aa26c7845b31406100fb28a59537b576
Erfan Abdi <erfangplus@gmail.com>
2019-07-02 04:36:01 +0300

    brightnessfix: more universal fix
    Signed-off-by: Erfan Abdi <erfangplus@gmail.com>

--- prebuilt/9/brightnessfix/make.sh
+++ prebuilt/9/brightnessfix/make.sh
@@ -15,7 +15,28 @@ $scriptsdir/oat2dex.sh "$systempath/framework" "$systempath/framework/services.j
 mkdir -p "$TMPDIR/original_dex"
 7z e "$systempath/framework/services.jar" classes.dex -o"$TMPDIR/original_dex"
 java -jar "$BAKSMALIJAR" disassemble "$TMPDIR/original_dex/classes.dex" -o "$TMPDIR/dexout"
-cp -fpr "$LOCALDIR"/com_android_server_lights/* "$TMPDIR"/dexout/com/android/server/lights/
+#cp -fpr "$LOCALDIR"/com_android_server_lights/* "$TMPDIR"/dexout/com/android/server/lights/
+LightImpl=$(grep  ".method private setLightLocked(IIIII)V" "$TMPDIR"/dexout/com/android/server/lights/ -ri | cut -d ":" -f 1)
+if [[ $LightImpl == "" ]]; then
+    echo "ERR: Check 2: Patching Light not supported for this rom"
+    rm -rf "$TMPDIR"
+    exit
+fi
+cp $LightImpl "$TMPDIR"/LightImpl.smali
+rm $LightImpl
+flag=true
+while IFS= read -r line;
+do
+    $flag && echo "$line" >> $LightImpl
+    if [[ "$line" == ".method public setBrightness(II)V" ]]; then
+        flag=false
+        cat "$LOCALDIR"/LightsService_LightImpl.patch >> $LightImpl
+    fi
+    if ! $flag && [[ "$line" == ".end method" ]]; then
+        flag=true
+        echo "$line" >> $LightImpl
+    fi
+done  < "$TMPDIR"/LightImpl.smali
 java -jar "$SMALIJAR" assemble "$TMPDIR/dexout" -o "$TMPDIR/classes.dex"
 zip -gjq "$systempath/framework/services.jar" "$TMPDIR/classes.dex"
 java -jar $toolsdir/apktool/apktool.jar d $systempath/framework/framework-res.apk -o $TMPDIR/framework-res
