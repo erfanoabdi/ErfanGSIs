339cb399f22cb0c588cf4b0dcc8943a96a04146e
Erfan Abdi <erfangplus@gmail.com>
2019-07-07 02:41:54 +0300

    brightnessfix: also patch multiple classes jars
    * drop old method and add LightsService.java as source
    
    Signed-off-by: Erfan Abdi <erfangplus@gmail.com>

---
+++ prebuilt/9/brightnessfix/com_android_server_lights/LightsService.java
@@ -0,0 +1,265 @@
+/* * Copyright (C) 2008 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.server.lights;
+
+import com.android.server.SystemService;
+
+import android.app.ActivityManager;
+import android.content.Context;
+import android.os.Handler;
+import android.os.Message;
+import android.os.SystemProperties;
+import android.os.Trace;
+import android.provider.Settings;
+import android.util.Slog;
+
+public class LightsService extends SystemService {
+    static final String TAG = "LightsService";
+    static final boolean DEBUG = false;
+
+    final LightImpl mLights[] = new LightImpl[LightsManager.LIGHT_ID_COUNT];
+
+    private final class LightImpl extends Light {
+
+        private LightImpl(int id) {
+            mId = id;
+        }
+
+        @Override
+        public void setBrightness(int brightness) {
+            setBrightness(brightness, BRIGHTNESS_MODE_USER);
+        }
+
+        @Override
+        public void setBrightness(int brightness, int brightnessMode) {
+            synchronized (this) {
+                // LOW_PERSISTENCE cannot be manually set
+                if (brightnessMode == BRIGHTNESS_MODE_LOW_PERSISTENCE) {
+                    Slog.w(TAG, "setBrightness with LOW_PERSISTENCE unexpected #" + mId +
+                            ": brightness=0x" + Integer.toHexString(brightness));
+                    return;
+                }
+
+                int rom_max_brightness = SystemProperties.getInt("persist.display.rom_max_brightness", 255);
+                if(mId == 0) {
+                    String fp = SystemProperties.get("ro.vendor.build.fingerprint", "hello");
+                    if(fp.matches(".*astarqlte.*")) {
+                        int newBrightness = (int) (brightness / (rom_max_brightness / 255.0));
+                        if(SystemProperties.getBoolean("persist.sys.samsung.full_brightness", false)) {
+                            newBrightness = (int) (brightness * 365.0 / 255.0);
+                        }
+                        setLightLocked(newBrightness, LIGHT_FLASH_HARDWARE, 0, 0, brightnessMode);
+                        return;
+                    }
+
+                    if(SystemProperties.getInt("persist.sys.phh.samsung_backlight", 0) == 1 ||
+			            fp.matches(".*beyond.*lte.*") ||
+			            fp.matches(".*(crown|star)[q2]*lte.*") ||
+				    fp.matches(".*(SC-0[23]K|SCV3[89]).*")) {
+                        int newBrightness = (int) (brightness * 100 / (rom_max_brightness / 255.0));
+                        if(SystemProperties.getBoolean("persist.sys.samsung.full_brightness", false)) {
+                            newBrightness = (int) (brightness * 40960.0 / 255.0);
+                        }
+                        setLightLocked(newBrightness, LIGHT_FLASH_HARDWARE, 0, 0, brightnessMode);
+                        return;
+                    }
+
+                    boolean qcomExtendBrightness = SystemProperties.getBoolean("persist.extend.brightness", false);
+                    int scale = SystemProperties.getInt("persist.display.max_brightness", 1023);
+                    //This is set by vndk-detect
+                    int qcomScale = SystemProperties.getInt("persist.sys.qcom-brightness", -1);
+                    if(qcomScale != -1) {
+                        qcomExtendBrightness = true;
+                        scale = qcomScale;
+                    }
+
+                    if(qcomExtendBrightness) {
+                        int newBrightness = (int) (brightness * scale / rom_max_brightness);
+                        if (newBrightness > scale)
+                            newBrightness = scale;
+                        setLightLocked(newBrightness, LIGHT_FLASH_NONE, 0, 0, brightnessMode);
+                        return;
+                    }
+                }
+
+                int color = (int) (brightness / (rom_max_brightness / 255.0));
+                if (color > 255)
+                    color = 255;
+                if (color < 5)
+                    color = 5;
+                color = color & 0x000000ff;
+                color = 0xff000000 | (color << 16) | (color << 8) | color;
+                setLightLocked(color, LIGHT_FLASH_NONE, 0, 0, brightnessMode);
+            }
+        }
+
+        @Override
+        public void setColor(int color) {
+            synchronized (this) {
+                setLightLocked(color, LIGHT_FLASH_NONE, 0, 0, 0);
+            }
+        }
+
+        @Override
+        public void setFlashing(int color, int mode, int onMS, int offMS) {
+            synchronized (this) {
+                setLightLocked(color, mode, onMS, offMS, BRIGHTNESS_MODE_USER);
+            }
+        }
+
+        @Override
+        public void pulse() {
+            pulse(0x00ffffff, 7);
+        }
+
+        @Override
+        public void pulse(int color, int onMS) {
+            synchronized (this) {
+                if (mColor == 0 && !mFlashing) {
+                    setLightLocked(color, LIGHT_FLASH_HARDWARE, onMS, 1000,
+                            BRIGHTNESS_MODE_USER);
+                    mColor = 0;
+                    mH.sendMessageDelayed(Message.obtain(mH, 1, this), onMS);
+                }
+            }
+        }
+
+        @Override
+        public void turnOff() {
+            synchronized (this) {
+                setLightLocked(0, LIGHT_FLASH_NONE, 0, 0, 0);
+            }
+        }
+
+        @Override
+        public void setVrMode(boolean enabled) {
+            synchronized (this) {
+                if (mVrModeEnabled != enabled) {
+                    mVrModeEnabled = enabled;
+
+                    mUseLowPersistenceForVR =
+                            (getVrDisplayMode() == Settings.Secure.VR_DISPLAY_MODE_LOW_PERSISTENCE);
+                    if (shouldBeInLowPersistenceMode()) {
+                        mLastBrightnessMode = mBrightnessMode;
+                    }
+
+                    // NOTE: We do not trigger a call to setLightLocked here.  We do not know the
+                    // current brightness or other values when leaving VR so we avoid any incorrect
+                    // jumps. The code that calls this method will immediately issue a brightness
+                    // update which is when the change will occur.
+                }
+            }
+        }
+
+        private void stopFlashing() {
+            synchronized (this) {
+                setLightLocked(mColor, LIGHT_FLASH_NONE, 0, 0, BRIGHTNESS_MODE_USER);
+            }
+        }
+
+        private void setLightLocked(int color, int mode, int onMS, int offMS, int brightnessMode) {
+            if (shouldBeInLowPersistenceMode()) {
+                brightnessMode = BRIGHTNESS_MODE_LOW_PERSISTENCE;
+            } else if (brightnessMode == BRIGHTNESS_MODE_LOW_PERSISTENCE) {
+                brightnessMode = mLastBrightnessMode;
+            }
+
+            if (!mInitialized || color != mColor || mode != mMode || onMS != mOnMS ||
+                    offMS != mOffMS || mBrightnessMode != brightnessMode) {
+                if (DEBUG) Slog.v(TAG, "setLight #" + mId + ": color=#"
+                        + Integer.toHexString(color) + ": brightnessMode=" + brightnessMode);
+                mInitialized = true;
+                mLastColor = mColor;
+                mColor = color;
+                mMode = mode;
+                mOnMS = onMS;
+                mOffMS = offMS;
+                mBrightnessMode = brightnessMode;
+                Trace.traceBegin(Trace.TRACE_TAG_POWER, "setLight(" + mId + ", 0x"
+                        + Integer.toHexString(color) + ")");
+                try {
+                    setLight_native(mId, color, mode, onMS, offMS, brightnessMode);
+                } finally {
+                    Trace.traceEnd(Trace.TRACE_TAG_POWER);
+                }
+            }
+        }
+
+        private boolean shouldBeInLowPersistenceMode() {
+            return mVrModeEnabled && mUseLowPersistenceForVR;
+        }
+
+        private int mId;
+        private int mColor;
+        private int mMode;
+        private int mOnMS;
+        private int mOffMS;
+        private boolean mFlashing;
+        private int mBrightnessMode;
+        private int mLastBrightnessMode;
+        private int mLastColor;
+        private boolean mVrModeEnabled;
+        private boolean mUseLowPersistenceForVR;
+        private boolean mInitialized;
+    }
+
+    public LightsService(Context context) {
+        super(context);
+
+        for (int i = 0; i < LightsManager.LIGHT_ID_COUNT; i++) {
+            mLights[i] = new LightImpl(i);
+        }
+    }
+
+    @Override
+    public void onStart() {
+        publishLocalService(LightsManager.class, mService);
+    }
+
+    @Override
+    public void onBootPhase(int phase) {
+    }
+
+    private int getVrDisplayMode() {
+        int currentUser = ActivityManager.getCurrentUser();
+        return Settings.Secure.getIntForUser(getContext().getContentResolver(),
+                Settings.Secure.VR_DISPLAY_MODE,
+                /*default*/Settings.Secure.VR_DISPLAY_MODE_LOW_PERSISTENCE,
+                currentUser);
+    }
+
+    private final LightsManager mService = new LightsManager() {
+        @Override
+        public Light getLight(int id) {
+            if (0 <= id && id < LIGHT_ID_COUNT) {
+                return mLights[id];
+            } else {
+                return null;
+            }
+        }
+    };
+
+    private Handler mH = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            LightImpl light = (LightImpl)msg.obj;
+            light.stopFlashing();
+        }
+    };
+
+    static native void setLight_native(int light, int color, int mode,
+            int onMS, int offMS, int brightnessMode);
+}
--- prebuilt/9/brightnessfix/make.sh
+++ prebuilt/9/brightnessfix/make.sh
@@ -10,36 +10,43 @@ mkdir -p "$TMPDIR"
 
 BAKSMALIJAR="$toolsdir"/smali/baksmali.jar
 SMALIJAR="$toolsdir"/smali/smali.jar
+APKTOOL="$toolsdir"/apktool/apktool.jar
 
 $scriptsdir/oat2dex.sh "$systempath/framework" "$systempath/framework/services.jar"
 mkdir -p "$TMPDIR/original_dex"
-7z e "$systempath/framework/services.jar" classes.dex -o"$TMPDIR/original_dex"
-java -jar "$BAKSMALIJAR" disassemble "$TMPDIR/original_dex/classes.dex" -o "$TMPDIR/dexout"
-#cp -fpr "$LOCALDIR"/com_android_server_lights/* "$TMPDIR"/dexout/com/android/server/lights/
-LightImpl=$(grep  ".method private setLightLocked(IIIII)V" "$TMPDIR"/dexout/com/android/server/lights/ -ri | cut -d ":" -f 1)
-if [[ $LightImpl == "" ]]; then
-    echo "ERR: Check 2: Patching Light not supported for this rom"
-    rm -rf "$TMPDIR"
-    exit
-fi
-cp $LightImpl "$TMPDIR"/LightImpl.smali
-rm $LightImpl
-flag=true
-while IFS= read -r line;
-do
-    $flag && echo "$line" >> $LightImpl
-    if [[ "$line" == ".method public setBrightness(II)V" ]]; then
-        flag=false
-        cat "$LOCALDIR"/LightsService_LightImpl.patch >> $LightImpl
-    fi
-    if ! $flag && [[ "$line" == ".end method" ]]; then
+7z e "$systempath/framework/services.jar" classes* -o"$TMPDIR/original_dex" 2>/dev/null
+CLASSES=$(ls "$TMPDIR/original_dex/classes"*)
+for CLASS in $CLASSES; do
+    java -jar "$BAKSMALIJAR" disassemble "$CLASS" -o "$TMPDIR/dexout"
+    LightImpl=$(grep  ".method private setLightLocked(IIIII)V" "$TMPDIR"/dexout/com/android/server/lights/ -ri | cut -d ":" -f 1)
+    if [[ ! $LightImpl == "" ]]; then
+        cp $LightImpl "$TMPDIR"/LightImpl.smali
+        rm $LightImpl
         flag=true
-        echo "$line" >> $LightImpl
+        while IFS= read -r line;
+        do
+            $flag && echo "$line" >> $LightImpl
+            if [[ "$line" == ".method public setBrightness(II)V" ]]; then
+                flag=false
+                cat "$LOCALDIR"/LightsService_LightImpl.patch >> $LightImpl
+            fi
+            if ! $flag && [[ "$line" == ".end method" ]]; then
+                flag=true
+                echo "$line" >> $LightImpl
+            fi
+        done  < "$TMPDIR"/LightImpl.smali
+        NEWCLASS=$(echo "$CLASS" | rev | cut -d "/" -f 1 | rev)
+        java -jar "$SMALIJAR" assemble "$TMPDIR/dexout" -o "$TMPDIR/$NEWCLASS"
+        zip -gjq "$systempath/framework/services.jar" "$TMPDIR/$NEWCLASS"
+        java -jar "$APKTOOL" d $systempath/framework/framework-res.apk -o $TMPDIR/framework-res
+        ROMMAXBRIGHTNESS=$(grep '<integer name="config_screenBrightnessSettingMaximum">' $TMPDIR/framework-res/res/values/integers.xml | sed 's/<integer name="config_screenBrightnessSettingMaximum">//g' | sed 's/<\/integer>//' | sed 's/^[[:space:]]*//g')
+        echo "persist.display.rom_max_brightness=$ROMMAXBRIGHTNESS" >> $systempath/build.prop
+        rm -rf "$TMPDIR"
+        exit
     fi
-done  < "$TMPDIR"/LightImpl.smali
-java -jar "$SMALIJAR" assemble "$TMPDIR/dexout" -o "$TMPDIR/classes.dex"
-zip -gjq "$systempath/framework/services.jar" "$TMPDIR/classes.dex"
-java -jar $toolsdir/apktool/apktool.jar d $systempath/framework/framework-res.apk -o $TMPDIR/framework-res
-ROMMAXBRIGHTNESS=$(grep '<integer name="config_screenBrightnessSettingMaximum">' $TMPDIR/framework-res/res/values/integers.xml | sed 's/<integer name="config_screenBrightnessSettingMaximum">//g' | sed 's/<\/integer>//' | sed 's/^[[:space:]]*//g')
-echo "persist.display.rom_max_brightness=$ROMMAXBRIGHTNESS" >> $systempath/build.prop
+    rm -rf "$TMPDIR/dexout"
+done
+
+# This should not happen
+echo "ERR: Patching Light not supported for this rom"
 rm -rf "$TMPDIR"
