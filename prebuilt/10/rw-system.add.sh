65297822d18fee243cb7d73dac0b2da6525be89b
Erfan Abdi <erfangplus@gmail.com>
2019-07-07 03:36:29 +0300

    10: Q wifi fix not needed anymore
    Signed-off-by: Erfan Abdi <erfangplus@gmail.com>

--- prebuilt/10/rw-system.add.sh
+++ prebuilt/10/rw-system.add.sh
@@ -1,7 +1,3 @@
 mount -o bind /mnt/phh/empty_dir /vendor/overlay/DisplayCutoutEmulationTall || true
 mount -o bind /mnt/phh/empty_dir /vendor/overlay/DisplayCutoutEmulationDouble || true
 mount -o bind /mnt/phh/empty_dir /vendor/overlay/DisplayCutoutEmulationCorner || true
-
-if [ "$vndk" -ge 28 ]; then
-    mount -o bind /system/bin/wpa_supplicant /vendor/bin/hw/wpa_supplicant || true
-fi
