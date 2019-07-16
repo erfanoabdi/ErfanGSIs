03209ac016bed7982123040736dc2840682be2a8
Sludke <46661484+xdsurvivor@users.noreply.github.com>
2019-07-16 15:05:40 -0300

    Fix derp

--- roms/9/ZenUI/rw-system.add.sh
+++ roms/9/ZenUI/rw-system.add.sh
@@ -1,2 +1,2 @@
-model="$(sed -n 's/^ro.build.product=[[:space:]]*//p' /system/build.prop)"\
+model="$(sed -n 's/^ro.build.product=[[:space:]]*//p' /system/build.prop)"
 mount -o bind /vendor/etc/audio_policy_configuration.xml /vendor/etc/audio_policy_configuration_"$model".xml || true
