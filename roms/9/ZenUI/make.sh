4be13f836a561a7bd56ad952541630bc09cf2924
Erfan Abdi <erfangplus@gmail.com>
2019-07-17 12:01:25 +0300

    ZenUI: Audio fix
    Co-authored-by: xdsurvivor <sergio_ludke1@hotmail.com>
    Signed-off-by: Erfan Abdi <erfangplus@gmail.com>

--- roms/9/ZenUI/make.sh
+++ roms/9/ZenUI/make.sh
@@ -8,3 +8,13 @@ echo "ro.bluetooth.library_name=libbluetooth_qti.so" >> $1/build.prop
 
 # Custom files
 cp -fpr $thispath/lib64/* $1/lib64/
+
+# Fix audio
+model=$(sed -n 's/^ro.build.product=[[:space:]]*//p' "$1/build.prop")
+size=${#model}
+for n in $(seq $size);
+do
+    new=$new'\x00'
+done
+sed -i "s/audio_policy_configuration_$model.xml/audio_policy_configuration.xml\x00$new/" "$1/lib/libaudiopolicymanagerdefault.so"
+sed -i "s/audio_policy_configuration_$model.xml/audio_policy_configuration.xml\x00$new/" "$1/lib64/libaudiopolicymanagerdefault.so"
