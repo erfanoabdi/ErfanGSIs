c72c4e8315d29e5a5b85e77a697a67e48d2ad3fe
Erfan Abdi <erfangplus@gmail.com>
2019-07-17 12:18:03 +0300

    ZenUI: read model from lib for audio fix
    Signed-off-by: Erfan Abdi <erfangplus@gmail.com>

--- roms/9/ZenUI/make.sh
+++ roms/9/ZenUI/make.sh
@@ -10,11 +10,14 @@ echo "ro.bluetooth.library_name=libbluetooth_qti.so" >> $1/build.prop
 cp -fpr $thispath/lib64/* $1/lib64/
 
 # Fix audio
-model=$(sed -n 's/^ro.build.product=[[:space:]]*//p' "$1/build.prop")
-size=${#model}
-for n in $(seq $size);
+models=$(sed -n 's/audio_policy_configuration_*//p' "$1/lib/libaudiopolicymanagerdefault.so" | sed -n 's/.xml.*//p')
+for model in $models;
 do
-    new=$new'\x00'
+    size=${#model}
+    for n in $(seq $size);
+    do
+        new=$new'\x00'
+    done
+    sed -i "s/audio_policy_configuration_$model.xml/audio_policy_configuration.xml$new/" "$1/lib/libaudiopolicymanagerdefault.so"
+    sed -i "s/audio_policy_configuration_$model.xml/audio_policy_configuration.xml$new/" "$1/lib64/libaudiopolicymanagerdefault.so"
 done
-sed -i "s/audio_policy_configuration_$model.xml/audio_policy_configuration.xml\x00$new/" "$1/lib/libaudiopolicymanagerdefault.so"
-sed -i "s/audio_policy_configuration_$model.xml/audio_policy_configuration.xml\x00$new/" "$1/lib64/libaudiopolicymanagerdefault.so"
