8137a207d6f2748fbd6ba039a0581efc06cdf44c
amackpro <48667677+amackpro@users.noreply.github.com>
2019-07-22 23:50:50 +0300

    ZenUI: Update debloat.sh
    Signed-off-by: Erfan Abdi <erfangplus@gmail.com>

--- roms/9/ZenUI/debloat.sh
+++ roms/9/ZenUI/debloat.sh
@@ -5,12 +5,20 @@ thispath=`cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd`
 
 rm -rf $1/app/Drive
 rm -rf $1/app/Duo
+rm -rf $1/app/AsusFMRadio
+rm -rf $1/app/AsusFMService
+rm -rf $1/app/ATOKIME
+rm -rf $1/app/AudioWizard
+rm -rf $1/app/AudioWizardView
+rm -rf $1/app/BRApps
+rm -rf $1/app/Caping
 rm -rf $1/app/Maps
 rm -rf $1/app/YouTube
 rm -rf $1/app/talkback
 rm -rf $1/app/CalendarGoogle
 rm -rf $1/app/Photos
 rm -rf $1/app/GooglePay
+rm -rf $1/app/GameBroadcasterservice
 rm -rf $1/app/Music2
 rm -rf $1/app/Gmail2
 rm -rf $1/app/GoogleTTS
@@ -20,9 +28,16 @@ rm -rf $1/app/AsusFMService
 rm -rf $1/app/Facebook
 rm -rf $1/app/FacebookAppManager
 rm -rf $1/app/FacebookMessenger
+rm -rf $1/app/FlipfontMYingHei
+rm -rf $1/app/FlipfontMFinanceTW
+rm -rf $1/app/FlipfontMyuppyTW
 rm -rf $1/app/Instagram
 rm -rf $1/app/mangaDeals
+rm -rf $1/app/ZenUIHelp
 rm -rf $1/priv-app/YandexApp
+rm -rf $1/priv-app/MoterK
+rm -rf $1/priv-app/Motorservice
+rm -rf $1/priv-app/MyAsus
 rm -rf $1/priv-app/YandexBrowser
 rm -rf $1/priv-app/FacebookInstaller
 rm -rf $1/priv-app/FacebookNotificationServices
