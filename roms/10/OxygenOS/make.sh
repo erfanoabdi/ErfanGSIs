e420ffdd4e531a5015f8247adf07ce7523f4c188
Erfan Abdi <erfangplus@gmail.com>
2019-07-23 11:12:02 +0300

    10: OOS: Remove callrecording fix
    Signed-off-by: Erfan Abdi <erfangplus@gmail.com>

--- roms/10/OxygenOS/make.sh
+++ roms/10/OxygenOS/make.sh
@@ -4,7 +4,6 @@ systempath=$1
 thispath=`cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd`
 
 # AOSP libs
-cp -fpr $thispath/init/* $1/etc/init/
 cp -fpr $thispath/overlay/* $1/product/overlay/
 
 # build.prop
@@ -17,12 +16,6 @@ cp -fpr $thispath/overlay/* $1/product/overlay/
 #line 1 "device/qcom/sepolicy/qva/private/property_contexts"
 cp -fpr $thispath/selinux/* $1/etc/selinux/
 sed -i "/typetransition location_app/d" $1/etc/selinux/plat_sepolicy.cil
-
-# Enable OnePlus Call Recording
-sed -i "s/op_voice_recording_supported_by_mcc/op_voice_recording_supt_by_xxx/g" $1/priv-app/TeleService/TeleService.apk
-
-# drop caf permissions
-rm -rf $1/etc/permissions/qti_permissions.xml
 
 # fix bt audio for op gsi
 sed -i "/\/vendor\/etc\/audio/d" $1/bin/rw-system.sh
