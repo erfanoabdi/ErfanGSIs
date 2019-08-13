60038d656f50c8323bea42f7bdba83362e193ccd
Erfan Abdi <erfangplus@gmail.com>
2019-08-13 21:07:11 +0430

    MIUI10: Update debloat list
    Signed-off-by: Erfan Abdi <erfangplus@gmail.com>

--- roms/10/MIUI/debloat.sh
+++ roms/10/MIUI/debloat.sh
@@ -10,6 +10,21 @@ rm -rf $1/app/MiuiVideoGlobal
 rm -rf $1/app/Notes
 rm -rf $1/app/PaymentService
 rm -rf $1/app/Calculator
+rm -rf $1/app/AnalyticsCore
+rm -rf $1/app/BasicDreams
+rm -rf $1/app/BookmarkProvider
+rm -rf $1/app/MiuiBugReport
+rm -rf $1/app/CatchLog
+rm -rf $1/app/Mipay
+rm -rf $1/app/HybridAccessory
+rm -rf $1/app/HybridPlatform
+rm -rf $1/app/Joyose
+rm -rf $1/app/MiLinkService
+rm -rf $1/app/KSICibaEngine
+rm -rf $1/app/MiuiDaemon
+rm -rf $1/app/BuiltInPrintService
+rm -rf $1/app/PrintSpooler
+rm -rf $1/app/SoterService
 rm -rf $1/priv-app/Backup
 rm -rf $1/priv-app/Browser
 rm -rf $1/priv-app/Calendar
@@ -21,11 +36,5 @@ rm -rf $1/priv-app/Updater
 rm -rf $1/priv-app/Velvet
 rm -rf $1/priv-app/Weather
 rm -rf $1/priv-app/WeatherProvider
-
-#rm -rf $1/fonts/NotoSansEgyptianHieroglyphs-Regular.ttf
-#rm -rf $1/fonts/NotoSansTibetan-Bold.ttf
-#rm -rf $1/fonts/NotoSansTibetan-Regular.ttf
-#rm -rf $1/fonts/NotoSerifCJK-Regular.ttc
-#rm -rf $1/fonts/NotoSansCJK-Regular.ttc
-#rm -rf $1/fonts/NotoSansCuneiform-Regular.ttf
-#rm -rf $1/media/theme/miui_mod_icons
+rm -rf $1/priv-app/PersonalAssistant
+rm -rf $1/priv-app/BackupAndRestore
