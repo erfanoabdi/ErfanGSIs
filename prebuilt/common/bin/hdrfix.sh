f16cc651fc1821e1218b69a0e22af55e4c6f69eb
Erfan Abdi <erfangplus@gmail.com>
2019-08-25 20:57:04 +0430

    common: Finish hdrfix live patch
    Signed-off-by: Erfan Abdi <erfangplus@gmail.com>

--- prebuilt/common/bin/hdrfix.sh
+++ prebuilt/common/bin/hdrfix.sh
@@ -6,24 +6,31 @@
 # 2) Find isHDRLayer offset
 # 3) Find first "cbz" offset
 # 4) Patch it to "cbnz" and save to /data/local/tmp/
-# 5) Stop surfaceflinger and replaced patched lib
-# 6) Start surfaceflinger
+# 5) Stop surfaceflinger
+# 6) Patch lib
+# 7) Start surfaceflinger
 
-HDRLINE="08-24 18:53:40.892  7601  7601 F DEBUG   :     #00 pc 00000000000e75f8  /system/lib64/libsurfaceflinger.so (android::ExBufferLayer::isHDRLayer() const+48)"
-PATCHED="/Users/erfanabdi/Downloads/motosurface/libsurfaceflinger.so"
-#PATCHED="/data/local/tmp/libsurfaceflinger.so"
+DUMPSURF="/data/local/tmp/dumpsurf.txt"
 
 PATCH()
 {
     local HDROFFSET=$1
     local LIBSURFACEFLINGER=$2
-    local PATCHED=$3
-    echo $HDROFFSET $LIBSURFACEFLINGER $PATCHED
+    echo $HDROFFSET $LIBSURFACEFLINGER
+    objdump -d "$LIBSURFACEFLINGER" | grep " $HDROFFSET:" -B 7 > $DUMPSURF
+
+    if [ -z "$(cat $DUMPSURF | head -n 1 | grep cbz)" ]; then
+        echo "Live Patch not supported on this lib"
+        exit
+    fi
+    CBZ=$(cat $DUMPSURF | head -n 1 | awk '{ print "\\x"$2"\\x"$3"\\x"$4 }')
+    BL=$(cat $DUMPSURF | head -n 2 | tail -n 1 | awk '{ print "\\x"$2"\\x"$3"\\x"$4"\\x"$5 }')
+
+    sed -i "s|$CBZ\xb4$BL|$CBZ\xb5$BL|" "$LIBSURFACEFLINGER"
 }
 
 while true; do
-    #HDRLINE=$(adb logcat -d | grep isHDRLayer)
-    #HDRLINE=$(logcat -d | grep isHDRLayer)
+    HDRLINE=$(logcat -d | grep isHDRLayer)
     if [ -z "$HDRLINE" ]; then
         continue
     fi
@@ -31,17 +38,15 @@ while true; do
 done
 
 LIBSURFACEFLINGER=$(echo $HDRLINE | grep -o "/system/lib.*/libsurfaceflinger.so")
-HDROFFSET=$(echo $HDRLINE | sed "s|/system/lib.*/libsurfaceflinger.so.*||" | rev | awk '{ print $1 }' | rev)
-PATCH "$HDROFFSET" "$LIBSURFACEFLINGER" "$PATCHED"
-
+HDROFFSET=$(echo $HDRLINE | sed "s|/system/lib.*/libsurfaceflinger.so.*||" | rev | awk '{ print $1 }' | rev | sed 's/^0*//')
 stop surfaceflinger
 
 if mount -o remount,rw /system_root; then
-    cp -fpr "$PATCHED" "/system_root$LIBSURFACEFLINGER"
+    PATCH "$HDROFFSET" "/system_root$LIBSURFACEFLINGER"
     mount -o remount,ro /system_root || true
 fi
 if mount -o remount,rw /system; then
-    cp -fpr "$PATCHED" "/system$LIBSURFACEFLINGER"
+    PATCH "$HDROFFSET" "/system$LIBSURFACEFLINGER"
     mount -o remount,ro /system || true
 fi
 
