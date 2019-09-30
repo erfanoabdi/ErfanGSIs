a85cfb8d80574ec47d1cf11bbbd55d53813673fd
Azat Barış MIZRAKLI <barismizrakli@outlook.com>
2019-09-30 14:50:00 +0300

    Data read only fix
    Aosp Generic gsi some rom data read only fix

--- roms/10/Generic/make.sh
+++ roms/10/Generic/make.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
 # booooo
+systempath=$1
+thispath=`cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd`
+
+#Data read only fix > build.prop
+echo "ro.build.system_root_image=true" >> $1/build.prop
