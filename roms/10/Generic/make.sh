9cd3feb29ab4b9a3f5b88a51f67fc287532fbfc0
Azat Barış MIZRAKLI <barismizrakli@outlook.com>
2019-09-30 15:36:35 +0300

    AB Only
    Saw that some roms do not have permission to write internal storage

--- roms/10/Generic/make.sh
+++ roms/10/Generic/make.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+systempath=$1
+thispath=`cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd`
 
-# booooo
+# Saw that some roms do not have permission to write internal storage > build.prop
+echo "ro.build.system_root_image=true" >> $1/build.prop
