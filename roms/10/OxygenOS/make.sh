7964b5808af13380a08b4c219233293d8def3d2b
Shiv <shivpratapd1@gmail.com>
2019-10-11 21:52:08 +0530

    OOS 10: Fix default sounds/ringtones
    Just got bored and now doing all this.

--- roms/10/OxygenOS/make.sh
+++ roms/10/OxygenOS/make.sh
@@ -9,6 +9,12 @@ rsync -ra $thispath/system/ $systempath
 # Append file_context
 cat $thispath/file_contexts >> $1/etc/selinux/plat_file_contexts
 
+#Soundsssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss
+echo "ro.config.mms_notification=free.ogg" >> $1/etc/prop.default
+echo "ro.config.notification_sound=meet.ogg" >> $1/etc/prop.default
+echo "ro.config.alarm_alert=spring.ogg" >> $1/etc/prop.default
+echo "ro.config.ringtone=oneplus_tune.ogg" >> $1/etc/prop.default
+
 # fix bt audio for op gsi
 sed -i "/\/vendor\/etc\/audio /d" $1/bin/rw-system.sh
 
