88baeedf139725687f5094a7c747d220a50ebcb2
あきと ミズキト <peaktogo2@gmail.com>
2019-10-14 00:18:07 +0900

    Use Aria2
    Signed-off-by: Akito Mizukito <peak@echln.net>

--- setup.sh
+++ setup.sh
@@ -4,13 +4,13 @@ if [[ "$OSTYPE" == "linux-gnu" ]]; then
     distro=$(awk -F= '$1 == "ID" {print $2}' /etc/os-release)
     if [[ "$distro" == "arch" ]]; then
        echo "Arch Linux Detected"
-       sudo pacman -S unace unrar zip unzip p7zip sharutils uudeview arj cabextract file-roller dtc xz python-pip brotli lz4 gawk libmpack
+       sudo pacman -S unace unrar zip unzip p7zip sharutils uudeview arj cabextract file-roller dtc xz python-pip brotli lz4 gawk libmpack aria2
        #aur=rar
     else
-       sudo apt install unace unrar zip unzip p7zip-full p7zip-rar sharutils rar uudeview mpack arj cabextract file-roller device-tree-compiler liblzma-dev python-pip brotli liblz4-tool gawk
+       sudo apt install unace unrar zip unzip p7zip-full p7zip-rar sharutils rar uudeview mpack arj cabextract file-roller device-tree-compiler liblzma-dev python-pip brotli liblz4-tool gawk aria2
     fi
     pip install backports.lzma protobuf pycrypto
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-    brew install protobuf xz brotli lz4
+    brew install protobuf xz brotli lz4 aria2
     pip install backports.lzma protobuf pycrypto
 fi
