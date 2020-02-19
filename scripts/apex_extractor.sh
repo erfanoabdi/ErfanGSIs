f882c4e94159b2dbd1ed0b76db8f23449308aea2
Erfan Abdi <erfangplus@gmail.com>
2020-02-20 00:36:40 +0330

    apex_extractor: Check before extract

--- scripts/apex_extractor.sh
+++ scripts/apex_extractor.sh
@@ -8,6 +8,9 @@ APEXDIR="$1"
 APEXES=$(ls "$APEXDIR" | grep ".apex")
 for APEX in $APEXES; do
     APEXNAME=$(echo "$APEX" | sed 's/.apex//')
+    if [[ -d "$APEXDIR/$APEXNAME" ]]; then
+        continue
+    fi
     mkdir -p "$APEXDIR/$APEXNAME"
     7z e "$APEXDIR/$APEX" apex_payload.img apex_pubkey -o"$APEXDIR/$APEXNAME" 2>/dev/null >> "$TMPDIR"/zip.log
     7z x "$APEXDIR/$APEXNAME/apex_payload.img" -o"$APEXDIR/$APEXNAME" 2>/dev/null >> "$TMPDIR"/zip.log
