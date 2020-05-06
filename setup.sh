53e25342e24bedb097c2e413468628104850a4df
syanhai_yakisoba=\chan! <44153315+koumaza@users.noreply.github.com>
2020-05-06 21:32:10 +0900

    Update setup.sh
    --needed 
    If it is installed and there is no update, the package will not be reinstalled.

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ if [[ "$OSTYPE" == "linux-gnu" ]]; then
     distro=$(awk -F= '$1 == "ID" {print $2}' /etc/os-release)
     if [[ "$distro" == "arch" ]]; then
        echo "Arch Linux Detected"
-       sudo pacman -S unace unrar zip unzip p7zip sharutils uudeview arj cabextract file-roller dtc xz python-pip brotli lz4 gawk libmpack aria2
+       sudo pacman -S --needed unace unrar zip unzip p7zip sharutils uudeview arj cabextract file-roller dtc xz python-pip brotli lz4 gawk libmpack aria2
        #aur=rar
     else
        sudo apt install unace unrar zip unzip p7zip-full p7zip-rar sharutils rar uudeview mpack arj cabextract file-roller device-tree-compiler liblzma-dev python-pip brotli liblz4-tool gawk aria2
