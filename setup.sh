5e1532b3b1a632c905e55812efb85d0d1528f32b
fap <linuxbckp@gmail.com>
2020-05-07 20:00:33 +0800

    add Fedora support

--- setup.sh
+++ setup.sh
@@ -6,6 +6,9 @@ if [[ "$OSTYPE" == "linux-gnu" ]]; then
        echo "Arch Linux Detected"
        sudo pacman -S unace unrar zip unzip p7zip sharutils uudeview arj cabextract file-roller dtc xz python-pip brotli lz4 gawk libmpack aria2
        #aur=rar
+    else if [[ "$distro" == "fedora" ]]; then
+       echo "Fedora Detected"
+       sudo dnf install unace unrar zip unzip p7zip sharutils uudeview arj cabextract file-roller dtc xz python-pip brotli lz4 gawk \*mpack* aria2
     else
        sudo apt install unace unrar zip unzip p7zip-full p7zip-rar sharutils rar uudeview mpack arj cabextract file-roller device-tree-compiler liblzma-dev python-pip brotli liblz4-tool gawk aria2
     fi
