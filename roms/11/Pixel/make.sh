87ad607a6a32811275e79653ec4d43b2ac295104
Erfan Abdi <erfangplus@gmail.com>
2020-06-22 01:34:46 +0430

    Pixel11: dataservice_app moved to system_ext

--- roms/11/Pixel/make.sh
+++ roms/11/Pixel/make.sh
@@ -15,3 +15,4 @@ echo "persist.sys.overlay.pixelrecents=true" >> $1/etc/prop.default
 echo "qemu.hw.mainkeys=0" >> $1/etc/prop.default
 
 sed -i "/dataservice_app/d" $1/product/etc/selinux/product_seapp_contexts
+sed -i "/dataservice_app/d" $1/system_ext/etc/selinux/system_ext_seapp_contexts
