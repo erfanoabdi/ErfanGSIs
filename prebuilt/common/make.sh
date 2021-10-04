25c3b94bb2931a141242451cfd7cdfe66cbf9c4d
Velosh <daffetyxd@gmail.com>
2021-10-04 17:21:32 -0300

    prebuilt: common: Don't disable SetupWziard
    Signed-off-by: Velosh <daffetyxd@gmail.com>

--- prebuilt/common/make.sh
+++ prebuilt/common/make.sh
@@ -38,17 +38,6 @@ cat $thispath/build.prop >> $1/build.prop
 # Disable Actionable props
 sed -i "/ro.actionable_compatible_property.enabled/d" $1/etc/prop.default
 sed -i "/ro.actionable_compatible_property.enabled/d" $1/build.prop
-# disable any setupwizard mode
-sed -i "/ro.setupwizard.mode/d" $1/etc/prop.default
-sed -i "/ro.setupwizard.mode/d" $1/build.prop
-sed -i "/ro.setupwizard.mode/d" $1/product/build.prop
-sed -i "/ro.setupwizard.mode/d" $1/product/etc/build.prop
-if [[ -f $1/product/etc/build.prop ]]; then
-    echo "ro.setupwizard.mode=DISABLED" >> $1/product/etc/build.prop
-else
-    echo "ro.setupwizard.mode=DISABLED" >> $1/etc/prop.default
-    echo "ro.setupwizard.mode=DISABLED" >> $1/product/build.prop
-fi
 # Disable vndk lite
 if [[ -f $1/product/etc/build.prop ]]; then
     echo "ro.vndk.lite=false" >> $1/product/etc/build.prop
