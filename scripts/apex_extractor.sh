6db940ca07f34d1d85d9236b7e1a05c34b069303
Erfan Abdi <erfangplus@gmail.com>
2021-10-04 22:43:01 +0330

    scripts: Add capex support

--- scripts/apex_extractor.sh
+++ scripts/apex_extractor.sh
@@ -7,18 +7,23 @@ TMPDIR=$LOCALDIR/../tmp/apex_ext
 mkdir -p "$TMPDIR"
 
 APEXDIR="$1"
-APEXES=$(ls "$APEXDIR" | grep ".apex")
+APEXES=$(ls "$APEXDIR" | grep ".*apex")
 for APEX in $APEXES; do
-    APEXNAME=$(echo "$APEX" | sed 's/.apex//')
+    APEXNAME=$(echo "$APEX" | sed 's/\.[^.]*$//')
     if [[ -d "$APEXDIR/$APEXNAME" ]]; then
         continue
     fi
+    if [[ $APEX == *".capex" ]]; then
+        7z e -y "$APEXDIR/$APEX" original_apex -o"$APEXDIR" 2>/dev/null >> "$TMPDIR"/zip.log
+        mv -f "$APEXDIR/original_apex" "$APEXDIR/$APEXNAME.apex"
+        rm -rf "$APEXDIR/$APEX"
+    fi
     mkdir -p "$APEXDIR/$APEXNAME"
-    7z e "$APEXDIR/$APEX" apex_payload.img apex_pubkey -o"$APEXDIR/$APEXNAME" 2>/dev/null >> "$TMPDIR"/zip.log
+    7z e -y "$APEXDIR/$APEX" apex_payload.img apex_pubkey -o"$APEXDIR/$APEXNAME" 2>/dev/null >> "$TMPDIR"/zip.log
     $EXT4EXTRACT "$APEXDIR/$APEXNAME/apex_payload.img" -D "$APEXDIR/$APEXNAME" 2>/dev/null
     rm "$APEXDIR/$APEXNAME/apex_payload.img"
     rm -rf "$APEXDIR/$APEXNAME/lost+found"
-    rm "$APEXDIR/$APEX"
+    rm "$APEXDIR/$APEXNAME.apex"
 done
 
 rm -rf "$TMPDIR"
