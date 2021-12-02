2af99de65ec138a26ea10a3f0e9cad6cb180dad0
Velosh <daffetyxd@gmail.com>
2021-12-02 11:30:47 -0300

    prebuilt: 12: Extract apexes
    * Google's apex tool (deapexer.py, I mean) can deal with .apex/.capex (and it's symlink) fine, so there's no reason to have apex extractor script nuked.

--- prebuilt/12/make.sh
+++ prebuilt/12/make.sh
@@ -5,8 +5,8 @@ romdir=$2
 thispath=`cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd`
 
 # Deal with non-flattened apex
-#$thispath/../../scripts/apex_extractor.sh $1/apex
-#$thispath/../../scripts/apex_extractor.sh $1/system_ext/apex
+$thispath/../../scripts/apex_extractor.sh $1/apex
+$thispath/../../scripts/apex_extractor.sh $1/system_ext/apex
 echo "ro.apex.updatable=true" >> $1/product/etc/build.prop
 
 # Copy system files
